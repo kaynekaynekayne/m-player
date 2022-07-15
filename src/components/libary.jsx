c717de23469ab548be9345840040b25857b87ec4
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-07-15 15:47:24 +0900

    making media ui uncompleted

--- src/components/libary.jsx
+++ src/components/libary.jsx
@@ -5,7 +5,6 @@ const Library = ({songs, setCurrentSong, currentSong, openLibrary}) =>{
     
     return(
         <section className={`library ${openLibrary ? "open-library" : ""}`}>
-            <h2>Songs List</h2>
             <ul className="library-songs">
                 {songs.map(song=>
                     <LibrarySong
