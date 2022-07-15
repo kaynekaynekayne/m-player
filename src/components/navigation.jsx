3ae9ee93f442c9dafd64917fff9fd0d8ff657358
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-07-15 17:01:53 +0900

    add memo in navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{memo} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Navigation = ({setOpenLibrary}) => {
+const Navigation = memo(({setOpenLibrary}) => {
 
     const onOpenLibrary=()=>{
         setOpenLibrary(prev=>!prev)
@@ -16,6 +16,6 @@ const Navigation = ({setOpenLibrary}) => {
             </button>
         </nav>
     )
-};
+});
 
 export default Navigation;
